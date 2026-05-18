6e22e004bac0b8aa99c0bff986f12adb502f0f1f
WaylonOg <c27735305@gmail.com>
2026-05-18 10:35:31 -0500

    Update WalkSpeed.lua

--- Helpers/WalkSpeed.lua
+++ Helpers/WalkSpeed.lua
@@ -11,4 +11,21 @@ local Char = Plr.Character or Plr.CharacterAdded:Wait()
 local Hum = Char:WaitForChild("Humanoid")
 local WalkSpeed = Hum.WalkSpeed
 
-if getgenv().
+if getgenv().UseCFrame then
+          running = getgenv().UseCFrame
+        if running then
+            local char = workspace:FindFirstChild(game:GetService("Players").LocalPlayer.Name)
+            local hrp = char.HumanoidRootPart
+            local hum = char.Humanoid
+            task.spawn(function()
+                while running and char and hum and hrp do
+                    local dir = hum.MoveDirection
+                    if dir.Magnitude > 0 then
+                        hrp.CFrame = hrp.CFrame + (dir * speed)
+                    end
+                    task.wait() 
+                end
+            end)
+  end
+
+  
